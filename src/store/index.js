a61cf377d1bb89b8405eccb67d5a484f68990960
Magdalena Dusza <magdalena.dusza@infullmobile.com>
2020-01-08 17:41:56 +0100

    chat template

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,12 @@
-import Vue from 'vue';
-import Vuex from 'vuex';
-import VuexPersistence from 'vuex-persist';
-import mutations from './mutations';
-import actions from './actions';
+import Vue from "vue";
+import Vuex from "vuex";
+import VuexPersistence from "vuex-persist";
+import mutations from "./mutations";
+import actions from "./actions";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
-const debug = process.env.NODE_ENV !== 'prouction';
+const debug = process.env.NODE_ENV !== "prouction";
 const vuexLocal = new VuexPersistence({
   storage: window.localStorage
 });
@@ -15,13 +15,54 @@ export default new Vuex.Store({
   state: {
     loading: false,
     sending: false,
-    error: null,
-    user: [],
-    reonnect: false,
-    activeRoom: null,
-    rooms: [],
-    users: [],
-    messages: [],
+    error: "Relax! This is just a drill error message",
+    user: {
+      username: "Jack",
+      name: "Jack Sparrow"
+    },
+    reconnect: false,
+    activeRoom: {
+      id: "124"
+    },
+    rooms: [
+      {
+        id: "123",
+        name: "Ships"
+      },
+      {
+        id: "124",
+        name: "Treasure"
+      }
+    ],
+    users: [
+      {
+        username: "Jack",
+        name: "Jack Sparrow",
+        presence: "online"
+      },
+      {
+        username: "Barbossa",
+        name: "Hector Barbossa",
+        presence: "offline"
+      }
+    ],
+    messages: [
+      {
+        username: "Jack",
+        date: "11/12/1644",
+        text: "Not all treasure is silver and gold mate"
+      },
+      {
+        username: "Jack",
+        date: "12/12/1644",
+        text: "If you were waiting for the opportune moment, that was it"
+      },
+      {
+        username: "Hector",
+        date: "12/12/1644",
+        text: "You know Jack, I thought I had you figured out"
+      }
+    ],
     userTyping: null
   },
   mutations,
@@ -31,4 +72,4 @@ export default new Vuex.Store({
   },
   plugins: [vuexLocal.plugin],
   strict: debug
-})
+});
--- src/views/ChatDashboard.vue
+++ src/views/ChatDashboard.vue
@@ -1,3 +1,45 @@
 <template>
-    <p>chat dashboard</p>
+  <div class="chat-dashboard">
+    <ChatNavBar />
+    <b-container fluidclass="ld-over" v-bind:class="{running: loading}">
+      <div class="ld ld-spin ld-ring"></div>
+      <b-row>
+        <b-col cols="2">
+          <RoomList />
+        </b-col>
+        <b-col cols="7">
+          <b-row>
+            <b-col id="chat-content">
+              <MessageList />
+            </b-col>
+          </b-row>
+          <b-row>
+            <b-col>
+              <MessageForm />
+            </b-col>
+          </b-row>
+        </b-col>
+        <b-col cols="3">
+          <UserList />
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
 </template>
+
+<script>
+import ChatNavBar from "@/components/ChatNavBar.vue";
+import RoomList from "@/components/RoomList.vue";
+import MessageList from "@/components/MessageList.vue";
+import MessageForm from "@/components/MessageForm.vue";
+import UserList from "@/components/UserList.vue";
+import { mapState } from "vuex";
+
+export default {
+  name: "Chat",
+  components: { ChatNavBar, RoomList, MessageList, MessageForm, UserList },
+  computed: {
+    ...mapState(["loading"])
+  }
+};
+</script>
